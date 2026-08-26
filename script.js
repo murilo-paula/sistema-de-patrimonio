79e559177a46cc6551a645ed3c3dc1bb227dd114
unknown <suporte@B-TI-NB-70C0.tijucaalimentos.local>
2026-08-26 08:54:38 -0300

    Ajustes na sidebar

--- script.js
+++ script.js
@@ -2,11 +2,13 @@
 const btnmenu = document.getElementById("btnmenu");
 const sidebar = document.getElementById("sidebar");
 const main = document.getElementById("main");
+const headernav = document.getElementById("headernav");
 
 btnmenu.addEventListener('click', () => {
     console.log("AA")
     sidebar.classList.toggle("ativa");
     main.classList.toggle("ativa");
+    headernav.classList.toggle("ativa")
 
     if (sidebar.classList.contains("ativa")) {
         btnmenu.innerText = "✕";
